ad935c633333ef791391c039b1f1455758d3058d
jj <jjavierdguezas@gmail.com>
2018-08-19 12:41:09 +0200

    a lot of changes

--- assets/main.js
+++ assets/main.js
@@ -21,45 +21,16 @@ jQuery(document).ready(function($) {
 
     /* Bootstrap Tooltip for Skillset */
     $('.level-label').tooltip();
+    $('[data-toggle="tooltip"]').tooltip()
 
 
-    /* jQuery RSS - https://github.com/sdepold/jquery-rss */
-
-    // $("#rss-feeds").rss(
-
-    //     //Change this to your own rss feeds
-    //     "https://blog.apiad.net/feed.xml",
-
-    //     {
-    //     // how many entries do you want?
-    //     // default: 4
-    //     // valid values: any integer
-    //     limit: 4,
-
-    //     // the effect, which is used to let the entries appear
-    //     // default: 'show'
-    //     // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
-    //     effect: 'slideFastSynced',
-
-    //     // outer template for the html transformation
-    //     // default: "<ul>{entries}</ul>"
-    //     // valid values: any string
-    //     layoutTemplate: "<div class='item'>{entries}</div>",
-
-    //     // inner template for each entry
-    //     // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
-    //     // valid values: any string
-    //     entryTemplate: '<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
-
-    //     }
-    // );
-
-    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    // GitHubCalendar("#github-graph", "apiad");
-
-
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    // GitHubActivity.feed({ username: "apiad", selector: "#ghfeed" });
+    $("#jj-age").html(age(new Date(1993, 6, 21)));
 
 
 });
+
+function age(birthday)
+{
+  birthday = new Date(birthday);
+  return new Number((new Date().getTime() - birthday.getTime()) / 31536000000).toFixed(0);
+}
